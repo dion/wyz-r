03cbafa7829a5487f35fbaa666c9de60fca9bb49
Dion Coder <jellopops@gmail.com>
2024-02-21 21:41:38 -0500

    adding findProtein method, adding anchor tag to recipe/slug page
    adding container class to app div wrapper

--- frontend/components/Item.vue
+++ frontend/components/Item.vue
@@ -2,20 +2,28 @@
     <div class="container mx-auto">
       <div class="columns-3xs gap-8 space-y-8">
         <div v-for="(item, index) in recipes.data" :key="index" class="max-w-md mx-auto bg-white shadow-md rounded-md overflow-hidden">
-            <h2>{{ item.name }}</h2>
-            <p>{{ item.slug }}</p>
-            <p>{{ item.author_email }}</p>
-            <img :src="item.images[0]" />
+            <a :href="`/recipe/${item.slug}`">
+                <div>
+                    <h2>{{ item.name }}</h2>
+                    <p>{{ item.slug }}</p>
+                    <p>{{ item.author_email }}</p>
+                    <img :src="item.images[0]" />
+                    <p>{{ findFirstProtein(item.ingredients).name }}</p>
+                </div>
+            </a>
         </div>
       </div>
     </div>
 </template>
   
 <script setup>
-  const { data: recipes, pending, error, refresh } = await useFetch('http://localhost:8888/api/recipes', {});
+    const { data: recipes, pending, error, refresh } = await useFetch('http://localhost:8888/api/recipes', {});
 
-  console.log('recipes', recipes.value.data);
+    const findFirstProtein = function(arr) {
+        return arr.find(item => item.type === 'protein') || null;
+    }
+    
+    console.log('recipes', recipes.value.data);
 //   TODO: add infinite scroll
 // TODO: add link to recipe/[slug] page
-// TODO: return first ingredient that has type protein
 </script>
